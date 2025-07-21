d93b00adb89adffa2955c254a5912b08e8761ca7
mapaquin <mapaquin@mtl-vdi-wrk052l.rodeofx.com>
2025-07-21 12:06:35 -0400

    _cachedResolver

--- src/CachedResolver/module.cpp
+++ src/CachedResolver/module.cpp
@@ -4,7 +4,7 @@ void wrapResolver(pybind11::module_ &);
 void wrapResolverContext(pybind11::module_ &);
 void wrapResolverTokens(pybind11::module_ &);
 
-PYBIND11_MODULE(cachedResolver, m) {
+PYBIND11_MODULE(_cachedResolver, m) {
     wrapResolver(m);
     wrapResolverContext(m);
     wrapResolverTokens(m);
